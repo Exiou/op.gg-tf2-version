42c407965804bf52a4bc29c66723449634ec0100
Exiou <exiougaming@gmail.com>
2020-03-20 16:46:21 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const axios = require('axios')
+const SteamID = require('steamid')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/player/:sid64', async (req,res) => {
+    try{
+        const { sid64 } = req.params
+        let { classe = '', logs = 0 } = req.query
+
+        var kills = 0, deaths = 0, assists = 0, dmg = 0, dapm = 0, ubers = 0, drops = 0
+            
+        const a = await axios.get(`http://logs.tf/api/v1/log?player=${sid64}`)
+            
+            
+
+
+
+        var contador = 0
+
+        // verificar modo de jogo
+
+        for(let i = 0; i < logs;i++){
+        
+            const b = await axios.get(`http://logs.tf/api/v1/log/${a.data.logs[i].id}`)
+
+            var sid3 = new SteamID(sid64)
+
+            sid3 = sid3.getSteam3RenderedID()
+
+            const player = b.data.players[`${sid3}`]
+
+            if(classe == "medic" && player.class_stats[0].type == "medic"){
+
+                kills += player.kills
+                deaths += player.deaths
+                assists += player.assists
+                dmg += player.dmg
+                dapm += player.dapm
+                ubers += player.ubers
+                drops += player.drops
+                contador++
+
+            }else if(classe == "soldier" && player.class_stats[0].type == "soldier"){
+                kills += player.kills
+                deaths += player.deaths
+                assists += player.assists
+                dmg += player.dmg
+                dapm += player.dapm
+                contador++
+            }else if(classe == "scout" && player.class_stats[0].type == "scout"){
+                kills += player.kills
+                deaths += player.deaths
+                assists += player.assists
+                dmg += player.dmg
+                dapm += player.dapm
+                contador++
+            }else{
+                logs++
+            }
+
+        }
+
+        const total = {
+            kills,
+            deaths,
+            assists,
+            dmg,
+            dapm,
+            ubers,
+            drops,
+        }
+
+        const media = {
+            kills: kills/contador,
+            deaths: deaths/contador,
+            assists: assists/contador,
+            dmg: dmg/contador,
+            dapm: dapm/contador,
+            ubers: ubers/contador,
+            drops: drops/contador
+        }
+
+        console.log(logs)
+
+        res.json({ total, media, contador })
+    }catch(err){
+        res.send(err)
+    }
+
+})
+
+app.listen(3333)
