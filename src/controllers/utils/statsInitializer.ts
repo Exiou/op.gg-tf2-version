fc2463e6dc00bf2980ac0fb5871951fd7d45745a
Exiou <ryan.adoliveira23@gmail.com>
2020-04-10 17:17:29 -0300

    statsInitializer

---
+++ src/controllers/utils/statsInitializer.ts
@@ -0,0 +1,157 @@
+import { Stats } from "../../models/interfaces/PlayerInterface";
+
+export const statsInitializer: Stats = {
+  sixesStats: {
+    scoutStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    soldierStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    demomanStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    medicStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      kad: 0,
+      dmgTaken: 0,
+      ubers: 0,
+      drops: 0,
+      heal: 0,
+      avgTimeToBuild: 0,
+      avgUberLength: 0
+    },
+    scoutLogCount: 0,
+    soldierLogCount: 0,
+    demomanLogCount: 0,
+    medicLogCount: 0
+  },
+  highlanderStats: {
+    scoutStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    soldierStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    pyroStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    demomanStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    heavyStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    engineerStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0
+    },
+    medicStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      kad: 0,
+      dmgTaken: 0,
+      ubers: 0,
+      drops: 0,
+      heal: 0,
+      avgTimeToBuild: 0,
+      avgUberLength: 0
+    },
+    sniperStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0,
+      headshots: 0
+    },
+    spyStats: {
+      kills: 0,
+      assists: 0,
+      deaths: 0,
+      dmg: 0,
+      dpm: 0,
+      teamDmgRatio: 0,
+      kad: 0,
+      healReceived: 0,
+      backstabs: 0
+    },
+    scoutLogCount: 0,
+    soldierLogCount: 0,
+    pyroLogCount: 0,
+    demomanLogCount: 0,
+    heavyLogCount: 0,
+    engineerLogCount: 0,
+    medicLogCount: 0,
+    sniperLogCount: 0,
+    spyLogCount: 0
+  }
+}
